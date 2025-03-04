a9ac86c12af43389a7aaeba61425dbb9f2fc4fa1
Kevin Lam <klam@lcs.on.ca>
2025-03-04 07:02:23 -0500

    Initial Commit

---
+++ ComputedMVVM/ComputedMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputedMVVMApp.swift
+//  ComputedMVVM
+//
+//  Created by Kevin Lam on 4/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct ComputedMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
